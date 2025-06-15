d102637b8e32a4066a0ee6416bffcc0f0537790f
Artem <artem.vashenko201010@gmail.com>
2025-06-15 23:22:18 +0300

    Fixing bugs with DB

--- social_network/friends/views.py
+++ social_network/friends/views.py
@@ -13,8 +13,10 @@ class FriendsView(TemplateView):
         context = super(FriendsView, self).get_context_data(**kwargs)
         profile = Profile.objects.get(user = self.request.user)
         context["all_recommended"] = Profile.objects.filter().exclude(user = self.request.user)
-        context["all_friends"] = Friendship.objects.filter(profile2 = profile, accepted = True)
+        context["all_friends"] = Friendship.objects.filter(accepted = True)
         context["all_requests"] = Friendship.objects.filter(profile2 = profile, accepted = False)
+        context["all_avatars"] = Avatar.objects.all()
+        context["current_user"] = Profile.objects.get(user = self.request.user)
         return context 
     
     
@@ -23,8 +25,9 @@ class AllFriendsView(TemplateView):
     template_name = "all_friends/all_friends.html"
     def get_context_data(self, **kwargs):
         context = super(AllFriendsView, self).get_context_data(**kwargs)
-        user = Profile.objects.get(user = self.request.user)
-        context["all_friends"] = user.friends.all()
+        context["all_friends"] = Friendship.objects.filter(accepted = True)
+        context["all_avatars"] = Avatar.objects.all()
+        context["current_user"] = Profile.objects.get(user = self.request.user)
         return context 
 
 class RequestView(TemplateView):
@@ -32,22 +35,17 @@ class RequestView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(RequestView, self).get_context_data(**kwargs)
-        context["all_requests"] = Friendship.objects.filter(received_user = self.request.user.pk)
+        profile = Profile.objects.get(user = self.request.user)
+        context["all_requests"] = Friendship.objects.filter(profile2 = profile, accepted = False)
+        context["all_avatars"] = Avatar.objects.all()
         return context 
     
 class RecommendedView(TemplateView):
     template_name = "recommended/recommended.html"
     def get_context_data(self, **kwargs):
         context = super(RecommendedView, self).get_context_data(**kwargs)
-        # all_rejected_pks = DeclineRecommended.objects.filter(
-        #                                 current_user=self.request.user
-        #                             ).values_list('rejected_user__id', flat=True)
-        # all_profiles = Profile.objects.exclude(
-        #                                     user_id__in=all_rejected_pks
-        #                                 ).exclude(
-        #                                     user=self.request.user
-        #                                 )
-        # context["all_recommended"] = all_profiles
+        context["all_recommended"] = Profile.objects.filter().exclude(user = self.request.user)
+        context["all_avatars"] = Avatar.objects.all()
         return context
 
 class FriendProfileView(TemplateView):
@@ -61,14 +59,18 @@ class FriendProfileView(TemplateView):
         context['avatar'] = Avatar.objects.filter(profile = profile_id)
         context['all_avatars'] = Avatar.objects.all()
         context['all_posts'] = Post.objects.filter(author_id = profile_id.pk).all()
+
         context['current_request'] = Friendship.objects.get(profile1= Profile.objects.get(user = friend_user), profile2 =Profile.objects.get(user = self.request.user))
         return context
         
 
 def delete_request(request, pk):
-    request = Friendship.objects.get(sent_user_id = pk,
-                                       received_user = request.user)
-    request.delete()
+    current_user = Profile.objects.get(user_id = request.user.id)
+    friend_user = Profile.objects.get(user_id = pk)
+    if Friendship.objects.filter(profile1_id = request.user.pk, profile2 = friend_user):
+        Friendship.objects.get(profile1 = current_user, profile2 = friend_user).delete()
+    if Friendship.objects.filter(profile2 = current_user, profile1 = friend_user):
+        Friendship.objects.get(profile2 = current_user, profile1 = friend_user).delete()
     return redirect("main_friends")
 
 def delete_recommended(request, pk):
@@ -82,7 +84,10 @@ def delete_recommended(request, pk):
 def delete_friend(request, pk):
     current_user = Profile.objects.get(user_id = request.user.id)
     friend_user = Profile.objects.get(user_id = pk)
-    current_user.remove_friend(friend = friend_user)
+    if Friendship.objects.filter(profile1 = current_user, profile2 = friend_user):
+        Friendship.objects.get(profile1 = current_user, profile2 = friend_user).delete()
+    if Friendship.objects.filter(profile2 = current_user, profile1 = friend_user):
+        Friendship.objects.get(profile2 = current_user, profile1 = friend_user).delete()
     return redirect("main_friends")
 
 def confirm_friend(request, pk):    
@@ -90,15 +95,13 @@ def confirm_friend(request, pk):
     friend_profile = Profile.objects.get(user_id = pk)
     current_user = User.objects.get(id = request.user.id)
     friend_user = User.objects.get(id = pk)
-    current_profile.friends.add(friend_profile)
-    request = Friendship.objects.get(sent_user=friend_user, received_user=current_user)
+    request = Friendship.objects.get(profile1=friend_profile, profile2=current_profile)
     request.accepted = True
     request.save()
-    request.delete()
     return redirect("main_friends")
 
 def request_to_user(request, pk):
-    current_user = User.objects.get(id = request.user.id)
-    request_user = User.objects.get(id = pk)
-    Friendship.objects.create(sent_user = current_user, received_user = request_user, accepted = False)
+    current_user = Profile.objects.get(user_id = request.user.id)
+    request_user = Profile.objects.get(user_id = pk)
+    Friendship.objects.create(profile1 = current_user, profile2 = request_user, accepted = False)
     return redirect("main_friends")
